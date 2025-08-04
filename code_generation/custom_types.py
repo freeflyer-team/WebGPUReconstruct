372c94a5c7e8b1fb1964c59cc53aa584e0bdfae8
Albin Bernhardsson <catscratchesp@gmail.com>
2025-08-04 18:51:31 +0200

    Buffer in bind group entry
    https://github.com/gpuweb/gpuweb/pull/5193 allows using GPUBuffer as a GPUBindingResource. This previously had to be done using a GPUBufferBinding.

--- code_generation/custom_types.py
+++ code_generation/custom_types.py
@@ -28,24 +28,24 @@ class CustomType:
 
 GPUColor = CustomType("""
 if ($name.r != undefined) {
-__WebGPUReconstruct_file.writeFloat64($name.r);
+    __WebGPUReconstruct_file.writeFloat64($name.r);
 } else {
-__WebGPUReconstruct_file.writeFloat64($name[0]);
+    __WebGPUReconstruct_file.writeFloat64($name[0]);
 }
 if ($name.g != undefined) {
-__WebGPUReconstruct_file.writeFloat64($name.g);
+    __WebGPUReconstruct_file.writeFloat64($name.g);
 } else {
-__WebGPUReconstruct_file.writeFloat64($name[1]);
+    __WebGPUReconstruct_file.writeFloat64($name[1]);
 }
 if ($name.b != undefined) {
-__WebGPUReconstruct_file.writeFloat64($name.b);
+    __WebGPUReconstruct_file.writeFloat64($name.b);
 } else {
-__WebGPUReconstruct_file.writeFloat64($name[2]);
+    __WebGPUReconstruct_file.writeFloat64($name[2]);
 }
 if ($name.a != undefined) {
-__WebGPUReconstruct_file.writeFloat64($name.a);
+    __WebGPUReconstruct_file.writeFloat64($name.a);
 } else {
-__WebGPUReconstruct_file.writeFloat64($name[3]);
+    __WebGPUReconstruct_file.writeFloat64($name[3]);
 }
 """, """
 $name.r = reader.ReadFloat64();
@@ -57,25 +57,25 @@ $name.a = reader.ReadFloat64();
 
 GPUExtent3D = CustomType("""
 if ($name instanceof Array) {
-__WebGPUReconstruct_file.writeUint32($name[0]);
-if ($name.length < 2) {
-    $name.push(1);
-}
-__WebGPUReconstruct_file.writeUint32($name[1]);
-if ($name.length < 3) {
-    $name.push(1);
-}
-__WebGPUReconstruct_file.writeUint32($name[2]);
+    __WebGPUReconstruct_file.writeUint32($name[0]);
+    if ($name.length < 2) {
+        $name.push(1);
+    }
+    __WebGPUReconstruct_file.writeUint32($name[1]);
+    if ($name.length < 3) {
+        $name.push(1);
+    }
+    __WebGPUReconstruct_file.writeUint32($name[2]);
 } else {
-__WebGPUReconstruct_file.writeUint32($name.width);
-if ($name.height == undefined) {
-    $name.height = 1;
-}
-__WebGPUReconstruct_file.writeUint32($name.height);
-if ($name.depthOrArrayLayers == undefined) {
-    $name.depthOrArrayLayers = 1;
-}
-__WebGPUReconstruct_file.writeUint32($name.depthOrArrayLayers);
+    __WebGPUReconstruct_file.writeUint32($name.width);
+    if ($name.height == undefined) {
+        $name.height = 1;
+    }
+    __WebGPUReconstruct_file.writeUint32($name.height);
+    if ($name.depthOrArrayLayers == undefined) {
+        $name.depthOrArrayLayers = 1;
+    }
+    __WebGPUReconstruct_file.writeUint32($name.depthOrArrayLayers);
 }
 """, """
 $name.width = reader.ReadUint32();
@@ -86,13 +86,13 @@ $name.depthOrArrayLayers = reader.ReadUint32();
 
 GPUOrigin3D = CustomType("""
 if ($name instanceof Array) {
-__WebGPUReconstruct_file.writeUint32($name[0]);
-__WebGPUReconstruct_file.writeUint32($name[1]);
-__WebGPUReconstruct_file.writeUint32($name[2]);
+    __WebGPUReconstruct_file.writeUint32($name[0]);
+    __WebGPUReconstruct_file.writeUint32($name[1]);
+    __WebGPUReconstruct_file.writeUint32($name[2]);
 } else {
-__WebGPUReconstruct_file.writeUint32($name.x);
-__WebGPUReconstruct_file.writeUint32($name.y);
-__WebGPUReconstruct_file.writeUint32($name.z);
+    __WebGPUReconstruct_file.writeUint32($name.x);
+    __WebGPUReconstruct_file.writeUint32($name.y);
+    __WebGPUReconstruct_file.writeUint32($name.z);
 }
 """, """
 $name.x = reader.ReadUint32();
@@ -103,9 +103,9 @@ $name.z = reader.ReadUint32();
 
 Uint32DefaultMax = CustomType("""
 if ($name == undefined) {
-__WebGPUReconstruct_file.writeUint32(0xffffffff);
+    __WebGPUReconstruct_file.writeUint32(0xffffffff);
 } else {
-__WebGPUReconstruct_file.writeUint32($name);
+    __WebGPUReconstruct_file.writeUint32($name);
 }
 """,
 """
@@ -116,25 +116,32 @@ $name = reader.ReadUint32();
 GPUBindGroupEntry = CustomType("""
 __WebGPUReconstruct_file.writeUint32($name.binding);
 if ($name.resource instanceof GPUSampler) {
-__WebGPUReconstruct_file.writeUint8(0);
-__WebGPUReconstruct_file.writeUint32($name.resource.__id);
+    __WebGPUReconstruct_file.writeUint8(0);
+    __WebGPUReconstruct_file.writeUint32($name.resource.__id);
 } else if ($name.resource instanceof GPUTextureView) {
-__WebGPUReconstruct_file.writeUint8(1);
-__WebGPUReconstruct_file.writeUint32($name.resource.__id);
+    __WebGPUReconstruct_file.writeUint8(1);
+    __WebGPUReconstruct_file.writeUint32($name.resource.__id);
 } else if (typeof GPUExternalTexture !== 'undefined' && $name.resource instanceof GPUExternalTexture) {
-__WebGPUReconstruct_file.writeUint8(3);
-__WebGPUReconstruct_file.writeUint32($name.resource.__id);
+    __WebGPUReconstruct_file.writeUint8(3);
+    __WebGPUReconstruct_file.writeUint32($name.resource.__id);
+} else if ($name.resource instanceof GPUBuffer) {
+    __WebGPUReconstruct_file.writeUint8(2);
+    __WebGPUReconstruct_file.writeUint32($name.resource.__id);
+    __WebGPUReconstruct_file.writeUint64(0);
+    __WebGPUReconstruct_file.writeUint64($name.resource.size);
 } else {
-__WebGPUReconstruct_file.writeUint8(2);
-__WebGPUReconstruct_file.writeUint32($name.resource.buffer.__id);
-if ($name.resource.offset == undefined) {
-$name.resource.offset = 0;
-}
-__WebGPUReconstruct_file.writeUint64($name.resource.offset);
-if ($name.resource.size == undefined) {
-$name.resource.size = $name.resource.buffer.size - $name.resource.offset;
-}
-__WebGPUReconstruct_file.writeUint64($name.resource.size);
+    __WebGPUReconstruct_file.writeUint8(2);
+    __WebGPUReconstruct_file.writeUint32($name.resource.buffer.__id);
+    let offset = $name.resource.offset;
+    if (offset == undefined) {
+        offset = 0;
+    }
+    __WebGPUReconstruct_file.writeUint64(offset);
+    let size = $name.resource.size;
+    if (size == undefined) {
+        size = $name.resource.buffer.size - offset;
+    }
+    __WebGPUReconstruct_file.writeUint64(size);
 }
 """,
 """
@@ -143,52 +150,52 @@ $name.binding = reader.ReadUint32();
 const uint8_t type = reader.ReadUint8();
 switch (type) {
 case 0:
-$name.sampler = GetIdType(mapGPUSampler, reader.ReadUint32());
-break;
+    $name.sampler = GetIdType(mapGPUSampler, reader.ReadUint32());
+    break;
 case 1:
-$name.textureView = GetIdType(mapGPUTextureView, reader.ReadUint32());
-break;
+    $name.textureView = GetIdType(mapGPUTextureView, reader.ReadUint32());
+    break;
 case 2:
-$name.buffer = GetIdType(mapGPUBuffer, reader.ReadUint32());
-$name.offset = reader.ReadUint64();
-$name.size = reader.ReadUint64();
-break;
+    $name.buffer = GetIdType(mapGPUBuffer, reader.ReadUint32());
+    $name.offset = reader.ReadUint64();
+    $name.size = reader.ReadUint64();
+    break;
 case 3:
-reader.ReadUint32();
-ErrorOutput("External textures are not implemented for bind groups.\\n");
-exit(0);
-break;
+    reader.ReadUint32();
+    ErrorOutput("External textures are not implemented for bind groups.\\n");
+    exit(0);
+    break;
 default:
-ErrorOutput("Unknown resource type when creating bind group.\\n");
-exit(0);
+    ErrorOutput("Unknown resource type when creating bind group.\\n");
+    exit(0);
 }
 """, "WGPUBindGroupEntry $name;")
 GPUBindGroupEntry.nativeName = "WGPUBindGroupEntry"
 
 GPUConstants = CustomType("""
 if ($names == undefined) {
-__WebGPUReconstruct_file.writeUint64(0);
+    __WebGPUReconstruct_file.writeUint64(0);
 } else {
-let keys = Object.keys($names);
-__WebGPUReconstruct_file.writeUint64(keys.length);
-for (let i = 0; i < keys.length; i += 1) {
-""" + String.save("keys[i]") + """
-__WebGPUReconstruct_file.writeFloat64($names[keys[i]]);
-}
+    let keys = Object.keys($names);
+    __WebGPUReconstruct_file.writeUint64(keys.length);
+    for (let i = 0; i < keys.length; i += 1) {
+        """ + String.save("keys[i]") + """
+        __WebGPUReconstruct_file.writeFloat64($names[keys[i]]);
+    }
 }
 """, """
 $nameCount = reader.ReadUint64();
 WGPUConstantEntry* constants = new WGPUConstantEntry[$nameCount];
 for (uint64_t keyI = 0; keyI < $nameCount; ++keyI) {
-""" + String.load("constants[keyI].key") + """
-constants[keyI].value = reader.ReadFloat64();
+    """ + String.load("constants[keyI].key") + """
+    constants[keyI].value = reader.ReadFloat64();
 }
 $names = constants;
 """, 'assert(false);\n', '$name', """
 for (uint64_t keyI = 0; keyI < $nameCount; ++keyI) {
-if ($names[keyI].key.length > 0) {
-delete[] $names[keyI].key.data;
-}
+    if ($names[keyI].key.length > 0) {
+        delete[] $names[keyI].key.data;
+    }
 }
 delete[] $names;
 """)
